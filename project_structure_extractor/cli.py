4403073fd0228446d60e999f22979f908f5a3868
Hamed Haghjo <hamedhaghjo@hotmail.com>
2023-09-22 02:36:29 +0200

    Add cli and extractor

---
+++ project_structure_extractor/cli.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+import click
+
+from project_structure_extractor.extractor import extract_structure
+
+@click.command()
+@click.option('--path', default='.', help='Path to the project directory.')
+@click.option('--hidden-included', is_flag=True, help='Include hidden folders/files in the structure.')
+@click.option('--ignore', default='', help='Comma-separated list of files or folders to ignore.')
+def cli(path, hidden_included, ignore):
+    path = os.path.abspath(path)
+    project_name = os.path.basename(path)
+    suffix = "_with_hidden" if hidden_included else ""
+    output_file_path = os.path.join(path, f"{project_name}_structure{suffix}.txt")
+
+    ignore_list = [os.path.abspath(i.strip()) for i in ignore.split(',')] if ignore else []
+
+    with open(output_file_path, 'w', encoding='utf-8') as file_handle:
+        file_handle.write(f"{project_name}/\n")
+        extract_structure(path, file_handle, include_hidden=hidden_included, ignore_list=ignore_list)
+
+    click.echo(f"Project structure written to {output_file_path}")
+
+if __name__ == "__main__":
+    cli()
---
+++ project_structure_extractor/extractor.py
@@ -0,0 +1,27 @@
+def extract_structure(path, file_handle, prefix='', include_hidden=False, ignore_list=None):
+    if not ignore_list:
+        ignore_list = []
+
+    absolute_path = os.path.abspath(path)
+    if absolute_path in ignore_list:
+        return
+
+    if os.path.isdir(path):
+        items = sorted([item for item in os.listdir(path)
+                        if (include_hidden or not item.startswith('.'))
+                        and os.path.join(path, item) not in ignore_list])
+
+        for index, item in enumerate(items):
+            is_last = index == len(items) - 1
+            item_path = os.path.join(path, item)
+
+            line_prefix = '└── ' if is_last else '├── '
+
+            if os.path.isdir(item_path):
+                file_handle.write(f"{prefix}{line_prefix}{item}/\n")
+                new_prefix = prefix + ('    ' if is_last else '│   ')
+                extract_structure(item_path, file_handle, new_prefix, include_hidden, ignore_list)
+            else:
+                file_handle.write(f"{prefix}{line_prefix}{item}\n")
+    else:
+        file_handle.write(f"{prefix}└── {os.path.basename(path)}\n")
