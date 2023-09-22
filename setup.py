95fd50bd8b44f6218d757e5a59c62340f9fd5e38
Hamed Haghjo <hamedhaghjo@hotmail.com>
2023-09-22 02:53:11 +0200

    Add setup for packaging

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="project-structure-extractor",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "Click",
+    ],
+    entry_points='''
+        [console_scripts]
+        structure-extractor=project_structure_extractor.cli:cli
+    ''',
+    author="Hamed Haghjo",
+    author_email="hamedhaghjo@hotmail.com",
+    description="CLI tool to extract the structure of a project into a .txt file.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/RealWorga/project-structure-extractor",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
